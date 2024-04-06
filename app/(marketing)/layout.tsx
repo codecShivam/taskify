90fce96305bb4c3abb6ec8d9c74d22bbe398123f
codecShivam <codecshivam@gmail.com>
2024-04-06 10:54:01 +0530

    building homepage

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,13 @@
+export default function MarketingLayout(
+    { children }: {
+        children: React.ReactNode
+    }
+) {
+    return (
+        <div className="h-full bgslate-100">
+            <main className="pt-40 pb-20 bg-slate-100">
+                {children}
+            </main>
+        </div >
+    )
+}
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="text-sky-500">
-      <h1 className="text-2xl">Hello World!</h1>
-    </div>
-  );
-}
